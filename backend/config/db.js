f701a4f0467851074227855436f73e0a0279fc70
Fidel Maluto <fidel.maluto77@.com>
2026-05-15 01:11:20 +0100

    feat: Colocando a funcionalidade de abertura do sistema com um click na URL

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
 export const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '########',
+    password: 'Angola@123',
     database: 'projects'
 });
 
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import employee from './routes/employees.js';
+import path from 'path';
+import { fileURLToPath  } from 'url';
 
 const server = express();
 const port = 3030
 
+// Recriando o __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+server.use(express.static(path.join(__dirname, '../frontend/html')))
 server.use(express.json());
 server.use(cors());
+
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/html', 'index.html'))
+})
 server.use('/', employee);
 
 server.listen(port, () => {
